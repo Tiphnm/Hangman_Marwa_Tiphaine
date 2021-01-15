760d80e86cb4b0c0077de0261dbae6cce447c7ee
Tiphaine Minguet <tiphaineminguet@MacBook-Air-de-Tiphaine-2.local>
2021-01-15 14:53:38 +0100

    Ajout de la lecture externe de ma liste et du module logging

--- Hangman.py
+++ Hangman.py
@@ -4,5 +4,12 @@
 import random 
 import logging
 
-logging.basicConfig(filename= "")
+logging.basicConfig(filename='text.log', level=logging.INFO,
+                    format='%(asctime)s: %(name)s :%(levelname)s:%(message)s')
 
+logging.info('Reading file : start')
+my_file = open("word_list.txt", "r") 
+lines = my_file.readlines()
+list_word = [elem.strip() for elem in lines]
+logging.info('reading file : end')
+my_file.close()
