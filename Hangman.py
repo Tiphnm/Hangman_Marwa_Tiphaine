c72d87850cf62c19d5e63b05c7882deb4a515f26
Tiphaine Minguet <tiphaineminguet@MacBook-Air-de-Tiphaine-2.local>
2021-01-20 11:46:47 +0100

    Ajout d'une condition sur les underscore: attention aux espaces ... Version fini, manque dessin dans sript

--- Hangman.py
+++ Hangman.py
@@ -19,40 +19,62 @@ my_file.close()
 
 ###### Variables
 
+Game_is_on = True
+
 lives = 5
+
+hidden_list = []
 
 logging.info('Choosing random word : start')
 random_word = list(random.choice(list_word))
 
 print(random_word) #pour nous 
-#print(type(user_choice))  #pour nous 
-
-hidden_list=[]
+#print(type(user_choice))  #pour nous
 
 ###### Remplacing letter by "_"
 
-def underscore(chosen_word, mylist):
+def underscore(chosen_word, secret_list):
     for i in chosen_word :
-        i = "_ "
-        mylist.append(i)
-    return mylist
+        i = "_"
+        secret_list.append(i)
+    return secret_list
 
 underscore_word = underscore(random_word, hidden_list)
 print(underscore_word)
 
 
-def user_try(life, chosen_word, mylist):   
-    guess = raw_input("Choose a letter: ").lower()
+def user_try(life, chosen_word, secret_list): 
 
-    if guess not in chosen_word : 
-        life -= 1 
-        print("You choose %s... Sorry you lost a life: \n " %(guess) + "\nYou have %s lives remaining." %(life))
+    Game_is_on = True
 
-    else :
-        for i in range(len(chosen_word)):
+    while Game_is_on:
+        guess = raw_input("Choose a letter: ").lower()
+        
+        for i in range(len(secret_list)):
             letter = chosen_word[i]
             if guess == letter: 
-                mylist[i] = letter 
-        print(underscore_word)
+                secret_list[i]= letter 
+        print(secret_list)
+
+        if secret_list.count("_") == 0:
+            Game_is_on = False 
+            print("You win")
+    
+        if guess not in chosen_word: 
+            life -=1
+            print("You lost a life, you have %s life remaining" %(life)) 
+
+        if life == 0: 
+            Game_is_on = False
+            print("You are dead.")
+        
+        
+
+#mon user a des vies et il devine la bonne lettre: on remplace l'underscore en la lettre deviné 
+
+#mon user n'a plus de vie, le jeu s'arrete 
+
+#autre proposition: tant qu'il y a des underscore dans ma liste et que mes vies sont supérieuses à 0 
+            
 
 user_try(lives, random_word, underscore_word)
